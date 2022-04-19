5c558e676cf925d18072d55f9a8172c3d2cf5f0a
Mauregina <mauraregina@gmail.com>
2022-04-19 18:00:22 -0400

    add user authentication
    Signed-off-by: Mauregina <mauraregina@gmail.com>

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,22 @@
 const router = require('express').Router()
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 router.get('/', async(req, res)=> {
-    User.find({}, (err, data) => {
-        if (err) return res.send({error: 'Erro na consulta!'})
-        return res.send(data);
-    })
+    try{
+        const user =  await User.find()
+
+        res.status(200).json(user)
+    }    
+    catch{
+        res.status(500).json({error: error})        
+    }    
 })
 
 router.post('/', async(req, res)=>{
     const {user, password} = req.body
 
-    if (!user || !password) return res.send({error: 'Dados insuficientes!'})  
+    if (!user || !password) return res.status(422).json({error: 'Dados insuficientes!'})  
 
     try{
         const user_found = await User.findOne({user})
@@ -27,7 +32,30 @@ router.post('/', async(req, res)=>{
     } catch(error){
         res.status(500).json({error: error})
     }
-    // })
+})
+
+router.post('/auth', async(req, res) => {
+    const {user, password} = req.body
+
+    if (!user || !password) return res.status(422).json({error: 'Dados insuficientes!'}) 
+
+    try{
+        const user_found = await User.findOne({user}).select('+password')
+
+        if (!user_found) {
+            res.status(422).json({error: 'Usuário não registrado!'})
+            return
+        }
+
+        bcrypt.compare(password, user_found.password, (err, isValid)=>{
+            if (!isValid) return res.status(422).json({error: 'Senha inválida!'}) 
+            res.status(201).json({message: "Usuário ''" + user + "'' autenticado"})
+        });
+
+    } catch(error){
+        res.status(500).json({error: error})
+    }   
+
 })
 
 module.exports = router
